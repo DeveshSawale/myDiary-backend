ca31a35329e7299649b68bc932ec1b0e8cc1b38c
Devesh Sawale <im007dev@gmail.com>
2023-02-01 23:00:26 +0530

    express validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req,res)=>{   
-    const user = User(req.body);
-    user.save();
-    res.json(req.body)
+router.post('/',[
+    body('email',"Enter a valid email!").isEmail(),
+    body('password',"Password must be at least of 5 characters!").isLength({ min: 5 }),
+], (req,res)=>{  
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        password: req.body.password, 
+        email: req.body.email,  
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({error : "This email is already taken", msg : err.message})
+    });
 })
 
 module.exports = router
