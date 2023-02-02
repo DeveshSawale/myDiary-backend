0cb6133b42408e9094c8710ebeb06afdd7a368b5
Devesh Sawale <im007dev@gmail.com>
2023-02-02 11:03:26 +0530

    password hashing and token creation

--- routes/auth.js
+++ routes/auth.js
@@ -2,25 +2,53 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+require("dotenv").config();
 
-router.post('/',[
+router.post('/createuser',[
+    // express validation
     body('email',"Enter a valid email!").isEmail(),
     body('password',"Password must be at least of 5 characters!").isLength({ min: 5 }),
-], (req,res)=>{  
+], async (req,res)=> {  
     
+    // if express validator detects any error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-        name: req.body.name,
-        password: req.body.password, 
-        email: req.body.email,  
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-    res.json({error : "This email is already taken", msg : err.message})
-    });
+    try {
+        let user = await User.findOne({email : req.body.email});
+        
+        // entered email already exists in database
+        if(user){
+            return res.status(400).json({error : "This email is already registered"})
+        }
+        
+        const salt = await bcrypt.genSalt(10);
+        const passW = await bcrypt.hash(req.body.password,salt)
+
+        user = await User.create({
+            name: req.body.name,
+            password: passW, 
+            email: req.body.email,  
+        })
+        const data = {
+            user : {
+                id : user.id
+            }
+        }
+        const token = jwt.sign(data,process.env.secret)
+    
+        res.json({token})
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error occured")
+    }
+
+    
 })
 
 module.exports = router
